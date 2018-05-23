f146bb2838cf578f8f0b9dca50fe52a0b56c5213
Alex Devero <deveroalex@gmail.com>
2018-05-23 10:05:59 +0200

    Syntax cleanup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,75 +1,79 @@
-const ml = require('ml-regression');
-const csv = require('csvtojson');
+const csv = require('csvtojson')
+const ml = require('ml-regression')
+const path = require('path')
+
 const SLR = ml.SLR; // Simple Linear Regression
 
-const csvFilePath = './src/data/advertising.csv'; // Data
-let csvData = [], // parsed Data
-  X = [], // Input
-  y = []; // Output
+const csvFilePath = './src/data/advertising.csv' // Data
+let csvData = [] // parsed Data
+let X = [] // Input
+let y = [] // Output
 
-let regressionModel;
+let regressionModel
 
-const readline = require('readline'); // For user prompt to allow predictions
+const readline = require('readline') // For user prompt to allow predictions
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+  input: process.stdin,
+  output: process.stdout
+})
 
 csv()
-    .fromFile(csvFilePath)
-    .on('json', (jsonObj) => {
-        csvData.push(jsonObj);
-    })
-    .on('done', () => {
-        dressData(); // To get data points from JSON Objects
-        performRegression();
-    });
+  .fromFile(csvFilePath)
+  .on('json', (jsonObj) => {
+    csvData.push(jsonObj)
+  })
+  .on('done', () => {
+    dressData() // To get data points from JSON Objects
+    performRegression()
+  })
 
 function performRegression() {
-    regressionModel = new SLR(X, y); // Train the model on training data
-    console.log(regressionModel.toString(3));
-    predictOutput();
+  regressionModel = new SLR(X, y) // Train the model on training data
+
+  console.log(regressionModel.toString(3))
+
+  predictOutput()
 }
 
 function dressData() {
-    /**
-     * One row of the data object looks like:
-     * {
-     *   TV: "10",
-     *   Radio: "100",
-     *   Newspaper: "20",
-     *   "Sales": "1000"
-     * }
-     *
-     * Hence, while adding the data points,
-     * we need to parse the String value as a Float.
-     */
-    csvData.forEach((row) => {
-        X.push(f(row.Radio)); // or row['radio']
-        y.push(f(row.Sales)); // or row['sales']
-    });
+  /**
+   * One row of the data object looks like:
+   * {
+   *   TV: "10",
+   *   Radio: "100",
+   *   Newspaper: "20",
+   *   "Sales": "1000"
+   * }
+   *
+   * Hence, while adding the data points,
+   * we need to parse the String value as a Float.
+   */
+  csvData.forEach((row) => {
+    X.push(f(row.Radio)) // or row['radio']
+    y.push(f(row.Sales)) // or row['sales']
+  })
 }
 
 function f(s) {
-    return parseFloat(s);
+  return parseFloat(s)
 }
 
 function predictOutput() {
   rl.question('Enter input X for prediction (Press CTRL+C to exit) : ', (answer) => {
-    console.log(`At X = ${answer}, y =  ${regressionModel.predict(parseFloat(answer)).toFixed(2)}`);
+    console.log(`At X = ${answer}, y =  ${regressionModel.predict(parseFloat(answer)).toFixed(2)}`)
 
     // Add ability to read from / write to files
-    const fs = require('fs');
-    const path = "./results.txt";
-    let data = `At X = ${answer}, Y =  ${regressionModel.predict(parseFloat(answer)).toFixed(2)}\r\n`; // \r\n are line breaks
+    const fs = require('fs')
+    const path = "./results.txt"
+    let data = `At X = ${answer}, Y =  ${regressionModel.predict(parseFloat(answer)).toFixed(2)}\r\n` // \r\n are line breaks
     const logStream = fs.createWriteStream(path, {'flags': 'a'}); // use {'flags': 'a'} to append and {'flags': 'w'} to erase and write a new file
 
     // Write results into txt file
-    logStream.write(data);
-    logStream.end('');
+    logStream.write(data)
+    logStream.end('')
 
     // Initiate predictOutput function
-    predictOutput();
-  });
+    predictOutput()
+  })
 }
